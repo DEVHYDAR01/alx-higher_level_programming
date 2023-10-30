4a33e79aae61bbd8396c7a52e62d7abf8a226b5e
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-10-30 23:21:57 +0100

    This commit will push edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[0:3]
+second = word[7:]
+middle = word[1:8]
+
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {second}")
+print(f"Middle word: {middle}")
