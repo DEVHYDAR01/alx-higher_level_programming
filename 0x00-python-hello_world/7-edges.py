55e0cbaa41c219f56f11ac74ee4077c6583b925e
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-10-30 23:27:41 +0100

    This commit will push a quick fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-first = word[0:3]
-second = word[7:]
-middle = word[1:8]
+word_first_3 = word[0:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
 
-print(f"First 3 letters: {first}")
-print(f"Last 2 letters: {second}")
-print(f"Middle word: {middle}")
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
