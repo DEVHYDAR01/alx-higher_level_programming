2d329aa26415c6ce1473f5a3338d1c629dd3d43f
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-03 03:32:46 +0100

    This commit will push codestyle fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    blt_operators = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(blt_operators.keys()):
+    myblt_ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(myblt_ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    print("{} {} {} = {}".format(a, sys.argv[2], b, blt_operators[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, myblt_ops[sys.argv[2]](a, b)))
