9ad74fa1c7251db762fa0d79d247d7bee06f7cb1
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-09 03:20:59 +0100

    This commit will ads

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    while val in a_dictionary.values():
+        for l, m in a_dictionary.items():
+            if m == value:
+                del a_dictionary[l]
+                break
+
+    return (a_dictionary)
