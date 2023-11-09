753d89fd1ea534e5e0b8854ed86c3633e632df31
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-09 05:43:42 +0100

    This commit will fixed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     while val in a_dictionary.values():
-        for l, m in a_dictionary.items():
+        for i, m in a_dictionary.items():
             if m == value:
-                del a_dictionary[l]
+                del a_dictionary[i]
                 break
 
     return (a_dictionary)
