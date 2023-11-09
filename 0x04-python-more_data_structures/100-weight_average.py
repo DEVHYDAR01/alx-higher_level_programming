d0d518db1525d2be4e4fc1ce8922fa8b459441df
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-09 03:23:08 +0100

    This commit will ads

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,6 @@ def weight_average(my_list=[]):
     ave = 0
     length = 0
     for p in my_list:
-        ave += (p[0] * t[1])
+        ave += (p[0] * p[1])
         length += (p[1])
-    return (ave / lenght)
+    return (ave / length)
