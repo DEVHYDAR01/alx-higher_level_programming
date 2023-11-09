da0622cfdebdae43fa42f1c9295bba352b4b962f
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-11-09 03:13:59 +0100

    This commit will bug fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,14 +6,14 @@ def roman_to_int(roman_string):
     roman_d = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     numb = 0
 
-    for lst in range(len(roman_string)):
-        if roman_d.get(roman_string[lst], 0) == 0:
+    for j in range(len(roman_string)):
+        if roman_d.get(roman_string[j], 0) == 0:
             return (0)
 
-        if (lst != (len(roman_string) - 1) and
-                roman_d[roman_string[lst]] < roman_d[roman_string[lst + 1]]):
-                    numb += roman_d[roman_string[lst]] * -1
+        if (j != (len(roman_string) - 1) and
+                roman_d[roman_string[j]] < roman_d[roman_string[j + 1]]):
+                    numb += roman_d[roman_string[j]] * -1
 
         else:
-            numb += roman_d[roman_string[lst]]
+            numb += roman_d[roman_string[j]]
     return (numb)
