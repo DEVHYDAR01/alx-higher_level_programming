db8480edccbd90be077b33e8de510ca7c3124266
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-03-11 10:58:16 +0100

    This commit will push task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     data = parse.urlencode(values).encode('utf-8')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as r:
-        print(r.read().decode('utf-8')
+        print(r.read().decode('utf-8'))
